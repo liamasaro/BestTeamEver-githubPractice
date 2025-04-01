949618bf654c5fa5717291300a6d092fe61580ab
liamasaro <lasaro@uvm.edu>
2025-04-01 16:50:02 -0400

    Update rgb_to_hex.cc

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -4,15 +4,15 @@
 
 std::string rgb_to_hex(int r, int g, int b)
 {
-    r = std::max(0, std::min(255, r));
-    g = std::max(0, std::min(255, g));
-    b = std::max(0, std::min(255, b));
+    r = std::max(0, std::min(255, r)); // red rgb values
+    g = std::max(0, std::min(255, g)); // green rgb values
+    b = std::max(0, std::min(255, b)); // blue rgb values
 
-    std::stringstream ss;
-    ss << std::uppercase << std::hex << std::setfill('0')
-       << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
+    std::stringstream ss; 
+    ss << std::uppercase << std::hex << std::setfill('0') 
+       << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b; // rgb to hex
 
-    return ss.str();
+    return ss.str(); // return color as a string
 }
 
 //Test with std::string hexColor = rgb_to_hex(255, 127, 0); // returns "FF7F00"
